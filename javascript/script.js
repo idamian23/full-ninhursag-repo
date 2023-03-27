7df746aab4070892354223c81ef4314f472e4ad6
idamian23 <damian.ionut23@yahoo.ro>
2023-03-27 11:41:36 +0300

    Prototypal inheritance

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,24 @@
-console.log('Hello world!');
+console.log('Hello world!')
 
-function Book(title, author, pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
 
-    this.info = function () {
-        return `${title} by ${author}, ${pages} pages, ${read}.`
-    }
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${read}.`
+  }
 }
 
-let myBook = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, 'not read yet');
+let myBook = new Book(
+  'The Great Gatsby',
+  'F. Scott Fitzgerald',
+  180,
+  'not read yet',
+)
 
-console.log(myBook.info());
+console.log(myBook.info())
 
 // All objects in JavaScript have a prototype. Stated simply, the prototype is another object that the original object inherits from,
 // which is to say, the original object has access to all of its prototype’s methods and properties.
@@ -23,11 +28,76 @@ function Student(name, grade) {
   this.grade = grade
 }
 
-Student.prototype.sayName = function() {
+Student.prototype.sayName = function () {
   console.log(this.name)
 }
-Student.prototype.goToProm = function() {
-  console.log("Eh.. go to prom?")
+Student.prototype.goToProm = function () {
+  console.log('Eh.. go to prom?')
 }
 
 // TO DO : read https://www.theodinproject.com/lessons/node-path-javascript-objects-and-object-constructors ASSIGNMENT
+
+const gimli = {
+  name: 'Gimli',
+  race: 'dwarf',
+  weapon: 'axe',
+  greet: function () {
+    return `Hi! My name is ${this.name}`
+  },
+}
+
+gimli.age = 39
+gimli['attack'] = function () {
+  return `Gimli attack with an ${this.weapon}!`
+}
+console.log(gimli.attack())
+//console.log(gimli)
+
+console.log(Object.keys(gimli))
+
+let x = {}
+
+console.log(Object.getPrototypeOf(x))
+
+let y = [1, 2, 3]
+console.log(Object.getPrototypeOf(y))
+
+function Hero(name, level) {
+  this.name = name
+  this.level = level
+}
+
+Hero.prototype.greet = function () {
+  return `${this.name} says Hello!`
+}
+
+function Warrior(name, level, weapon) {
+  Hero.call(this, name, level)
+  this.weapon = weapon
+}
+
+function Healer(name, level, spell) {
+  Hero.call(this, name, level)
+  this.spell = spell
+}
+
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype)
+Object.setPrototypeOf(Healer.prototype, Hero.prototype)
+
+// All other prototype methods added below
+
+Warrior.prototype.attack = function () {
+  return `${this.name} attacks with the ${this.weapon}.`
+}
+
+Healer.prototype.heal = function () {
+  return `${this.name} casts ${this.spell}.`
+}
+
+const hero1 = new Warrior('Bjorn', 1, 'axe')
+const hero2 = new Healer('Kanin', 1, 'cure')
+
+console.log(hero1.attack())
+console.log(hero1.greet())
+
+// TODO https://javascript.info/prototype-inheritance
